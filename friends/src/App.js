5da423b908db255f974e38020704143cc6eec47c
Cara Locke <cara.m.locke@gmail.com>
2021-07-20 00:51:36 -0400

    Successfully set up a PrivateRoute for FriendsList.

--- friends/src/App.js
+++ friends/src/App.js
@@ -2,19 +2,19 @@ import './App.css';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
 import FriendsList from './components/FriendsList';
 import Login from './components/Login';
-
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
+
   return (
     <Router>
       <div className="App">
         <header className="App-header">
            <Link to='/login'>Login</Link>
-           <Link>Logout</Link>
            <Link to='/friendslist'>Friends Page</Link>
         </header>
         <Switch>
-          <Route exact path='/friendslist' component={FriendsList} />
+          <PrivateRoute exact path='/friendslist' component={FriendsList} />
           <Route path='login' component={Login}/>
           <Route component={Login}/>
         </Switch>
